b6586734855669bc888984f1e2e822232a2313b6
Vansh Bordia <vanshbordia@gmail.com>
2022-02-09 23:35:36 +0530

    v22.2

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -386,11 +386,11 @@
  * Most modern mobile touch slider and framework with hardware accelerated transitions
  * http://swiperjs.com
  *
- * Copyright 2014-2020 Vladimir Kharlampidi
+ * Copyright 2014-2022 Vladimir Kharlampidi
  *
  * Released under the MIT License
  *
- * Released on: April 24, 2020
+ * Released on: April 24, 2022
  */
 
 
