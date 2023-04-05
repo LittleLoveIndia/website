83bb85aedfc4670b6c3ce4aad9ad4d8fea2a5c8a
Vansh Bordia <vanshbordia@gmail.com>
2023-04-05 16:17:03 +0530

    2023

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -386,11 +386,11 @@
  * Most modern mobile touch slider and framework with hardware accelerated transitions
  * http://swiperjs.com
  *
- * Copyright 2014-2022 Vladimir Kharlampidi
+ * Copyright 2014-2023 Vladimir Kharlampidi
  *
  * Released under the MIT License
  *
- * Released on: April 24, 2022
+ * Released on: April 24, 2023
  */
 
 
